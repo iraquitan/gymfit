0a4638554b1bb42734c87f5a76c6105d76256d75
Iraquitan Cordeiro Filho <iraquitanfilho@gmail.com>
2018-01-31 16:43:16 -0300

    feat: Add core and util apps

---
+++ gymfit/core/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+
+class RepetitionUnit(models.Model):
+    """
+    Setting unit, used in combination with an amount such as '10 reps', '5 km'
+    """
+    class Meta:
+        ordering = ["name", ]
+
+    name = models.CharField(max_length=100, verbose_name=_('Name'))
+
+    def __str__(self):
+        return self.name
+
+
+class WeightUnit(models.Model):
+    """
+    Weight unit, used in combination with an amount such as '10 kg', '5 plates'
+    """
+    class Meta:
+        ordering = ["name", ]
+
+    name = models.CharField(max_length=100, verbose_name=_('Name'))
+
+    def __str__(self):
+        return self.name
