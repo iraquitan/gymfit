0a5af01c0dbf2817655b6f20dc3f0e81a7bcb902
Iraquitan Cordeiro Filho <iraquitanfilho@gmail.com>
2018-01-31 18:21:51 -0300

    feat: Add initial exercises workout and core models

--- gymfit/exercises/models.py
+++ gymfit/exercises/models.py
@@ -1,12 +1,37 @@
+import uuid
+
+from django.core.validators import MinLengthValidator
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
 
-# Create your models here.
+class Muscle(models.Model):
+    """Model for Muscle"""
+    name = models.CharField(max_length=100, verbose_name=_("Name"))
+    is_front = models.BooleanField(default=1)
+
+    class Meta:
+        ordering = ["name", ]
+
+    def __str__(self):
+        return self.name
+
+
+class Equipment(models.Model):
+    """Model for Equipment"""
+    name = models.CharField(max_length=100, verbose_name=_("Name"))
+
+    def __str__(self):
+        return self.name
+
+
 class Category(models.Model):
     """Model for exercise category."""
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, verbose_name=_("Name"))
     description = models.TextField()
+
+    class Meta:
+        verbose_name_plural = _("Categories")
 
     def __str__(self):
         return self.name
@@ -14,13 +39,26 @@ class Category(models.Model):
 
 class Exercise(models.Model):
     """Model for exercise."""
-    name = models.CharField(max_length=100)
-    description = models.TextField()
-    weight = models.IntegerField()
-    reps = models.IntegerField()
-    series = models.IntegerField()
-    category = models.ForeignKey(Category, related_name='exercises',
-                                 on_delete=models.CASCADE)
+    name = models.CharField(max_length=100, verbose_name=_("Name"))
+    description = models.TextField(max_length=2000,
+                                   verbose_name=_('Description'),
+                                   validators=[MinLengthValidator(30)])
+    category = models.ForeignKey(Category, models.SET_NULL, blank=True,
+                                 null=True, verbose_name=_("Category"))
+    muscles = models.ManyToManyField(Muscle, blank=True,
+                                     verbose_name=_('Primary muscles'))
+    muscles_secondary = models.ManyToManyField(
+        Muscle, verbose_name=_('Secondary muscles'),
+        related_name='secondary_muscles', blank=True)
+    equipment = models.ManyToManyField(Equipment, verbose_name=_('Equipment'),
+                                       blank=True)
+    creation_date = models.DateField(_('Date'), auto_now_add=True, null=True,
+                                     blank=True)
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4,
+                            editable=False, verbose_name='UUID')
+
+    class Meta:
+        ordering = ["name", ]
 
     def __str__(self):
         return self.name
--- gymfit/workouts/models.py
+++ gymfit/workouts/models.py
@@ -1,3 +1,26 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+from gymfit.exercises.models import Exercise
+
+
+class Workout(models.Model):
+    """Model for Workout"""
+    name = models.CharField(max_length=100, verbose_name=_("Name"))
+    creation_date = models.DateField(_('Creation date'), auto_now_add=True)
+    comment = models.CharField(
+        verbose_name=_('Description'), max_length=100, blank=True,
+        help_text=_("A short description or goal of the workout. For "
+                    "example 'Focus on back' or 'Week 1 of program xy'.")
+    )
+    user = models.ForeignKey(User, verbose_name=_('User'))
+
+
+class WorkoutEntry(models.Model):
+    """Model for WorkoutEntry"""
+    workout = models.ForeignKey(Workout, verbose_name=_("Workout"))
+    exercise = models.ForeignKey(Exercise, verbose_name=_("Exercise"))
+    weight = models.IntegerField()
+    reps = models.IntegerField()
+    series = models.IntegerField()
